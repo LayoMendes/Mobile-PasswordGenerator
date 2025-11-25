28289384c16788b106165824f28540e864b17703
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-11-25 20:28:47 +0000

    25 minutos de aula

---
+++ passwordgenerator/app/App.tsx
@@ -0,0 +1,77 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image, Text, View, TouchableOpacity } from 'react-native';
+import Slider from '@react-native-community/slider'
+import React from 'react';
+import { useState } from 'react';
+
+export default function App() {
+
+  const [size, setSize] = useState(10)
+
+
+
+  return (
+    <View style={styles.container}>
+       <Image  source={require("../app/src/img/logopassword.png")} style={styles.logo}/>
+      <Text style={styles.title}>20 Caracteres</Text>
+      <View style={styles.area}>
+      
+
+      <Slider style={{height: 50}} 
+      minimumValue={6}
+      maximumValue={20}
+      maximumTrackTintColor='#ff0000'
+      minimumTrackTintColor='#000'
+      thumbTintColor='#392de9'
+      />
+      </View >
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f3f3ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginBottom: 60,
+
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 8,
+  },
+  button:{
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18,
+
+  },
+
+  buttonText:{
+    color: "#FFF",
+    fontSize: 20,
+
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+
+
+  }
+});
